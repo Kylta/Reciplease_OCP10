a106a19555d390595f698b6455b94a1756679041
Kylta <Christophe.bugnon02@gmail.com>
2019-03-19 20:46:57 +0100

    Create the Recipe model

---
+++ Reciplease/Reciplease/Recipe.swift
@@ -0,0 +1,19 @@
+//
+//  Recipe.swift
+//  Reciplease
+//
+//  Created by Christophe Bugnon on 19/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    let name: String
+    let ingredientsList: [String]
+    let like: String
+    let time: String
+    let image: String
+    let id: String?
+    let rate: Int
+}
