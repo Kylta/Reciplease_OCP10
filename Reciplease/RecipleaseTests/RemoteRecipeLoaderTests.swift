f643324d2d29a1ebca811c8586144c631a937a47
Kylta <Christophe.bugnon02@gmail.com>
2019-03-20 10:43:41 +0100

    Delivers invalidData error on non 200 HTTP responses

--- Reciplease/RecipleaseTests/RemoteRecipeLoaderTests.swift
+++ Reciplease/RecipleaseTests/RemoteRecipeLoaderTests.swift
@@ -14,6 +14,7 @@ class RemoteRecipeLoader {
 
     enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
     init(url: URL, client: HTTPClient) {
@@ -22,24 +23,33 @@ class RemoteRecipeLoader {
     }
 
     func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { _ in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
 
 class HTTPClient {
-    var messages = [(url: URL, completion: (Error) -> Void)]()
+    var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
     var requestedURLs: [URL] {
         return messages.map { $0.url }
     }
 
-    func get(from url: URL, completion: @escaping (Error) -> Void) {
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
         messages.append((url, completion))
     }
 
-    func complete(with error: Error) {
-        messages[0].completion(error)
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(error, nil)
+    }
+
+    func complete(withStatusCode code: Int, at index: Int = 0) {
+        let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+        messages[index].completion(nil, response)
     }
 }
 
@@ -79,6 +89,20 @@ class RemoteRecipeLoaderTests: XCTestCase {
         client.complete(with: clientError)
 
         XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let samples = [199, 201, 300, 400, 500]
+
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteRecipeLoader.Error]()
+
+            sut.load { capturedErrors.append($0) }
+            client.complete(withStatusCode: code, at: index)
+
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
 
     func makeSUT(url: URL = URL(string: "any-url.com")!) -> (sut: RemoteRecipeLoader, client: HTTPClient) {
