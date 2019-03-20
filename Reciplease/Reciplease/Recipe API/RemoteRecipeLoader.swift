65e23a1ab21fefedbcb01f1e3f4e4c0a92fcd615
Kylta <Christophe.bugnon02@gmail.com>
2019-03-20 15:34:29 +0100

    Made `RemoteRecipeLoader` to public

--- Reciplease/Reciplease/Recipe API/RemoteRecipeLoader.swift
+++ Reciplease/Reciplease/Recipe API/RemoteRecipeLoader.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class RemoteRecipeLoader: RecipeLoader {
+public final class RemoteRecipeLoader: RecipeLoader {
     private let url: URL
     private let client: HTTPClient
 
