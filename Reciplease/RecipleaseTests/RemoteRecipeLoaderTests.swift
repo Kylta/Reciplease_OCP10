15448ccfcf6669205dd60b8b2ddd4fc9195d24d8
Kylta <Christophe.bugnon02@gmail.com>
2019-03-20 14:59:42 +0100

    Move `HTTPClientSpy` to the bottom of the file

--- Reciplease/RecipleaseTests/RemoteRecipeLoaderTests.swift
+++ Reciplease/RecipleaseTests/RemoteRecipeLoaderTests.swift
@@ -9,26 +9,6 @@
 import XCTest
 import Reciplease
 
-private class HTTPClientSpy: HTTPClient {
-    var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-    var requestedURLs: [URL] {
-        return messages.map { $0.url }
-    }
-
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        messages.append((url, completion))
-    }
-
-    func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(.failure(error))
-    }
-
-    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-        let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-        messages[index].completion(.success(data, response))
-    }
-}
-
 class RemoteRecipeLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
@@ -130,5 +110,25 @@ class RemoteRecipeLoaderTests: XCTestCase {
         action()
 
         XCTAssertEqual(capturedResults, [result], file: file, line: line)
+    }
+
+    fileprivate class HTTPClientSpy: HTTPClient {
+        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            messages.append((url, completion))
+        }
+
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
+            messages[index].completion(.success(data, response))
+        }
     }
 }
