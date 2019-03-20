d55d42eb8ded35d163b149dd8aa10b0da41aa8fe
Kylta <Christophe.bugnon02@gmail.com>
2019-03-20 14:56:50 +0100

    Remove `RecipeItemMapper` to its own file

---
+++ Reciplease/Reciplease/RecipeItemMapper.swift
@@ -0,0 +1,65 @@
+//
+//  RecipeItemMapper.swift
+//  Reciplease
+//
+//  Created by Christophe Bugnon on 20/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
+
+internal struct RecipeItemMapper: Decodable {
+    private struct Root: Decodable {
+        let items: [RecipeItemMapper]
+
+        private enum CodingKeys: String, CodingKey {
+            case items = "matches"
+        }
+    }
+
+    let name: String
+    let ingredients: [String]
+    let id: String?
+    let rate: Int
+    let time: Int
+    let imageURL: URL
+    var item: Recipe {
+        return Recipe(name: name, ingredients: ingredients, id: id, rate: rate, time: time, imageURL: imageURL)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case ingredients, id, imageUrlsBySize
+        case name = "recipeName"
+        case rate = "rating"
+        case time = "totalTimeInSeconds"
+    }
+
+    private enum ImageURLCodingKeys: String, CodingKey {
+        case imageURL = "90"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let imageURLContainer = try container.nestedContainer(keyedBy: ImageURLCodingKeys.self, forKey: .imageUrlsBySize)
+
+        name = try container.decode(String.self, forKey: .name)
+        ingredients = try container.decode([String].self, forKey: .ingredients)
+        rate = try container.decode(Int.self, forKey: .rate)
+        time = try container.decode(Int.self, forKey: .time)
+
+        id = try container.decodeIfPresent(String.self, forKey: .id)
+
+        imageURL = try imageURLContainer.decode(URL.self, forKey: .imageURL)
+    }
+
+    static var OK_200: Int { return 200 }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [Recipe] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteRecipeLoader.Error.invalidData
+        }
+
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
+    }
+}
---
+++ Reciplease/Reciplease/RemoteRecipeLoader.swift
@@ -0,0 +1,45 @@
+//
+//  RemoteRecipeLoader.swift
+//  Reciplease
+//
+//  Created by Christophe Bugnon on 20/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
+
+public class RemoteRecipeLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public enum Result: Equatable {
+        case success([Recipe])
+        case failure(Error)
+    }
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .success(data, response):
+                do {
+                    let items = try RecipeItemMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
+                    completion(.failure(.invalidData))
+                }
+            case .failure:
+                completion(.failure(.connectivity))
+            }
+        }
+    }
+}
